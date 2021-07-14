269f9c6879766c704b7a0fc3486346bd226721c1
Ackesnal <ackesnal@foxmail.com>
2021-07-14 13:17:20 +1000

    updated

--- exec.sh
+++ exec.sh
@@ -4,9 +4,9 @@
 #SBATCH --job-name=Ackesnal
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:tesla-smx2:4
-#SBATCH --mem-per-cpu=20G
-#SBATCH -o tiny_batchsize512_[357911]_out.txt
+#SBATCH --gres=gpu:tesla-smx2:2
+#SBATCH --mem-per-cpu=10G
+#SBATCH -o tiny_batchsize512_[2,4,8,16]_out.txt
 #SBATCH -e err.txt
 
 source activate swin
@@ -14,4 +14,4 @@ module load cuda/10.1.243
 module load gnu7/7.3.0
 module load mvapich2
 
-srun python -m torch.distributed.launch --nproc_per_node 4 --master_port 10062 main.py --cfg configs/swin_test_tiny.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 160 --window-size=[3,5,7,9,11] 
+srun python -m torch.distributed.launch --nproc_per_node 2 --master_port 10162 main.py --cfg configs/swin_test_tiny.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 160 --window-size=[2,4,8,16] --output ./output/debug
