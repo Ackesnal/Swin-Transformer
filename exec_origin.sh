f77b5c8fc281944e77931f6da3563e4870bf889c
Ackesnal <ackesnal@foxmail.com>
2021-07-27 13:32:19 +1000

    updated

---
+++ exec_origin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --job-name=Ackesnal
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:tesla-smx2:2
+#SBATCH --mem-per-cpu=10G
+#SBATCH -o tiny_original_out.txt
+#SBATCH -e tiny_original_err.txt
+
+source activate swin
+module load cuda/10.1.243
+module load gnu7/7.3.0
+module load mvapich2
+
+srun python -m torch.distributed.launch --nproc_per_node 2 --master_port 10162 main.py --cfg configs/swin_test_tiny.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 256 --output ./output/original
