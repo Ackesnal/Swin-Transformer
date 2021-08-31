a7116babd183d5e42ac96de5b7374610f7f54c1d
Ackesnal <ackesnal@foxmail.com>
2021-08-31 16:38:31 +1000

    updated

--- exec_config.sh
+++ exec_config.sh
@@ -9,10 +9,8 @@
 #SBATCH -o tiny_multi_attn_50epoch_out.txt
 #SBATCH -e tiny_multi_attn_50epoch_err.txt
 
-source activate swin
 module load cuda/10.1.243
 module load gnu7/7.3.0
 module load mvapich2
 
-git checkout multi-attn
 srun python -m torch.distributed.launch --nproc_per_node 2 main.py --cfg configs/swin_test_tiny.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 256 --multi-attn true --output ./output/tiny_multi_attn_50epoch_out
