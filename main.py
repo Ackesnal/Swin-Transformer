1f7d4e8ffcb75fdcdbc34f8f24869bd3bdc588ef
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-10-01 16:12:48 +1000

    Update main.py

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ def train_one_epoch(config, model, criteria, data_loader, optimizer, epoch, mixu
             softmax = torch.nn.Softmax(dim=1)
             for i in range(4):
                 loss = loss + criteria[i+1](logsoftmax(outputs[i+1]), softmax(outputs[0]/4)) * 0.25"""
-            loss = reduce_tensor(loss)
+            loss
             optimizer.zero_grad()
             if config.AMP_OPT_LEVEL != "O0":
                 with amp.scale_loss(loss, optimizer) as scaled_loss:
@@ -225,6 +225,8 @@ def train_one_epoch(config, model, criteria, data_loader, optimizer, epoch, mixu
                 else:
                     grad_norm = get_grad_norm(amp.master_params(optimizer))
             else:
+                if 'SLURM_NTASKS' in os.environ:
+                    loss = loss / int(os.environ['SLURM_NTASKS'])
                 loss.backward()
                 if config.TRAIN.CLIP_GRAD:
                     grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), config.TRAIN.CLIP_GRAD)
