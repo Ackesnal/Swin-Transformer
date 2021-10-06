4a510b6773eddbf7e95bd1cbc9d85fac7dc8b5da
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-10-07 01:09:33 +1000

    Update swin_transformer.py

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -160,7 +160,7 @@ class WindowAttention(nn.Module):
             self.proj_drop = nn.Dropout(proj_drop, inplace=True)
         elif self.mode == 3:
             self.mlp = Mlp(in_features=dim, hidden_features=dim * 3, drop=proj_drop)
-         or self.mode == 4:
+        elif self.mode == 4:
             self.mlp = Mlp(in_features=dim, hidden_features=dim, drop=proj_drop)
             
     def forward(self, x, mask=None):
