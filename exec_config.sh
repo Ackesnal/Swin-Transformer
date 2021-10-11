e7d8dd6bbd2d51356c692f9b8f01f4cb750d08c1
Ackesnal <ackesnal@foxmail.com>
2021-10-11 17:57:53 +1000

    u

--- exec_config.sh
+++ exec_config.sh
@@ -13,4 +13,4 @@ module load cuda/10.1.243
 module load gnu7/7.3.0
 module load mvapich2
 
-srun python -m torch.distributed.launch --nproc_per_node 3 main.py --cfg configs/swin_test_10M.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 384 --multi-attn true --output ./output/tiny_multi_wd_4unit_1x1conv_10M_300epoch --use-checkpoint
+srun python -m torch.distributed.launch --nproc_per_node 3 main.py --cfg configs/swin_test_10M.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 256 --multi-attn true --output ./output/tiny_multi_wd_4unit_1x1conv_10M_300epoch --use-checkpoint
