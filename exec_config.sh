505bbfdd981fefc45d55be5e875fca6cf5924fed
Ackesnal <ackesnal@foxmail.com>
2021-10-18 00:57:17 +1000

    updated

--- exec_config.sh
+++ exec_config.sh
@@ -3,8 +3,8 @@
 #SBATCH -n 1
 #SBATCH --job-name=Ackesnal
 #SBATCH --partition=gpu
-#SBATCH --cpus-per-task=12
-#SBATCH --gres=gpu:tesla-smx2:3
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:tesla-smx2:2
 #SBATCH --mem-per-cpu=10G
 #SBATCH -o tiny_multi_attn_4unit_7M_edited_300epoch_out.txt
 #SBATCH -e tiny_multi_attn_4unit_7M_edited_300epoch_err.txt
@@ -13,4 +13,4 @@ module load cuda/10.1.243
 module load gnu7/7.3.0
 module load mvapich2
 
-srun python -m torch.distributed.launch --nproc_per_node=3 main.py --cfg configs/swin_test_7M.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 768 --multi-attn true --output ./output/tiny_multi_attn_4unit_7M_edited_300epoch --use-checkpoint
+srun python -m torch.distributed.launch --nproc_per_node=2 main.py --cfg configs/swin_test_7M.yaml --data-path ../BossNAS/data/imagenet/ --batch-size 512 --multi-attn true --output ./output/tiny_multi_attn_4unit_7M_edited_300epoch --use-checkpoint
