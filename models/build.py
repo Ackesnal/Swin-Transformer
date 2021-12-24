24863afa4d9264982410d24757a557966051080f
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2021-12-24 12:13:39 +1000

    Update build.py

--- models/build.py
+++ models/build.py
@@ -29,8 +29,6 @@ def build_model(config):
                                 ape=config.MODEL.SWIN.APE,
                                 patch_norm=config.MODEL.SWIN.PATCH_NORM,
                                 use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-                                multi_attn=config.MODEL.SWIN.MULTI_ATTN,
-                                same_attn=config.MODEL.SWIN.SAME_ATTN)
     elif model_type == 'swin_mlp':
         model = SwinMLP(img_size=config.DATA.IMG_SIZE,
                         patch_size=config.MODEL.SWIN_MLP.PATCH_SIZE,
@@ -46,7 +44,7 @@ def build_model(config):
                         ape=config.MODEL.SWIN_MLP.APE,
                         patch_norm=config.MODEL.SWIN_MLP.PATCH_NORM,
                         use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-                        multi_attn=config.MODEL.SWIN_MLP.MULTI_ATTN)
+                        shuffle=config.MODEL.SHUFFLE,)
     else:
         raise NotImplementedError(f"Unkown model: {model_type}")
 
