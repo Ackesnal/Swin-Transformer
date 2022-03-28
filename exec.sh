c8e339b8e87ecf21e748802829f6bcb6520ee478
Ackesnal <ackesnal@foxmail.com>
2022-03-28 18:15:06 +1000

    updated

--- exec.sh
+++ exec.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:tesla-smx2:2
 #SBATCH --mem-per-cpu=10G
-#SBATCH -o swin_tiny_shufflev2_300epoch_ratio0.4_kd_out.txt
-#SBATCH -e swin_tiny_shufflev2_300epoch_ratio0.4_kd_err.txt
+#SBATCH -o swin_tiny_shufflev2_300epoch_ratio0.5_kd_out.txt
+#SBATCH -e swin_tiny_shufflev2_300epoch_ratio0.5_kd_err.txt
 
-srun python -m torch.distributed.launch --nproc_per_node 2 --master_port 10452 main.py --cfg configs/swin_tiny_patch4_window7_224.yaml --data-path ../data/imagenet/ --batch-size 512 --use-checkpoint --output ./output/swin_tiny_shufflev2_300epoch_ratio0.4_kd
+srun python -m torch.distributed.launch --nproc_per_node 2 --master_port 10412 main.py --cfg configs/swin_tiny_patch4_window7_224.yaml --data-path ../data/imagenet/ --batch-size 512 --use-checkpoint --output ./output/swin_tiny_shufflev2_300epoch_ratio0.5_kd
